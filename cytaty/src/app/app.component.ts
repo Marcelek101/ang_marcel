a60b82e31cce3f335caf5c6711129cb27441ce4d
Marcelek101 <wojciechowskimarcel92@gmail.com>
2025-01-09 14:16:56 +0100

    cytaty

--- cytaty/src/app/app.component.ts
+++ cytaty/src/app/app.component.ts
@@ -15,9 +15,17 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   showForm = false;
   quotes: Quotation[] = QUOTES;
+  quotation: Quotation = { author: '', sentence: '', votes: 0 };
 
   onSwitchForm(): void {
     this.showForm = !this.showForm;
   }
-  title = 'cytaty';
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = { author: '', sentence: '', votes: 0 };
+  }
+
+  addVote(quotation: Quotation, value: number) {
+    quotation.votes += value;
+  }
 }
